91acaf1ce0b348201e25db3746b910338cee90f8
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2023-09-15 19:26:56 +0530

    Time: 82 ms (75.26%) | Memory: 17.6 MB (94.90%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,34 +1,29 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if target not in nums:
-            return [-1, -1]
-
-        # Find the leftmost occurrence of the target
-        left = self.findLeft(nums, target)
-
-        # Find the rightmost occurrence of the target
-        right = self.findRight(nums, target)
-
-        return [left, right]
-
-    def findLeft(self, nums, target):
+            return [-1,-1]
+        # Find leftmost occurrence
         low = 0
         high = len(nums) - 1
-        while low < high:
-            mid = low + (high - low) // 2
-            if nums[mid] < target:
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                start = mid
+                end = mid
+
+                # Find the start of the range
+                while start > 0 and nums[start - 1] == target:
+                    start -= 1
+
+                # Find the end of the range
+                while end < len(nums) - 1 and nums[end + 1] == target:
+                    end += 1
+
+                return [start, end]
+
+            elif target > nums[mid]:
                 low = mid + 1
             else:
-                high = mid
-        return low if nums[low] == target else -1
-
-    def findRight(self, nums, target):
-        low = 0
-        high = len(nums) - 1
-        while low < high:
-            mid = low + (high - low + 1) // 2  # Ensure mid biases towards the right
-            if nums[mid] > target:
                 high = mid - 1
-            else:
-                low = mid
-        return low if nums[low] == target else -1
+
+        return [-1, -1]
