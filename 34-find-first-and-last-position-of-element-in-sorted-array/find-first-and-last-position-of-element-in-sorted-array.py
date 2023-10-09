2285d0386aa48275ca3c70137750449c446ac085
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2023-10-09 19:40:24 +0530

    Time: 92 ms (11.73%) | Memory: 17.6 MB (97.83%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,28 +2,22 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if target not in nums:
             return [-1,-1]
-        # Find leftmost occurrence
-        low = 0
-        high = len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target:
-                start = mid
-                end = mid
-
-                # Find the start of the range
-                while start > 0 and nums[start - 1] == target:
-                    start -= 1
-
-                # Find the end of the range
-                while end < len(nums) - 1 and nums[end + 1] == target:
-                    end += 1
-
-                return [start, end]
-
-            elif target > nums[mid]:
-                low = mid + 1
+        
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                start=mid
+                end=mid
+                while start>0 and nums[start-1]==target:
+                    start-=1
+                while end<len(nums)-1 and nums[end+1]==target:
+                    end+=1
+                return [start,end]
+            elif nums[mid]<target:
+                low=mid+1
             else:
-                high = mid - 1
+                high=mid-1
+        return [-1,-1]
 
-        return [-1, -1]
