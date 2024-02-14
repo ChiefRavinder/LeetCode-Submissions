db3980df7b35be998049c46f55f9bc2666541d76
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2024-02-15 03:22:44 +0530

    Time: 62 ms (87.56%) | Memory: 16.7 MB (84.84%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,23 +1,26 @@
 class Solution:
-    def dfs(self,r,c,img,new,old_color,new_color,n,m):
-        new[r][c]=new_color
-        dr=[-1,0,1,0]
-        dc=[0,1,0,-1]
+    def dfs(self,image,new,sr,sc,color,m,n,pre_col,vis):
+        new[sr][sc]=color
+        dr=[0,1,-1,0]
+        dc=[1,0,0,-1]
         for i in range(4):
-            nr=r+dr[i]
-            nc=c+dc[i]
-            if 0<=nr<n and 0<=nc<m and img[nr][nc]==old_color and new[nr][nc]!=new_color:
-                self.dfs(nr,nc,img,new,old_color,new_color,n,m)
-
+            newr=sr+dr[i]
+            newc=sc+dc[i]
+            if 0<=newr<m and 0<=newc<n and vis[newr][newc]!=1 and image[newr][newc]==pre_col:
+                vis[newr][newc]=1
+                new[newr][newc]=color
+                self.dfs(image,new,newr,newc,color,m,n,pre_col,vis)
+                
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        n=len(image)
-        m=len(image[0])
-        new=[[0]*m for _ in range(n)]
-        in_color=image[sr][sc]
-        for i in range(n):
-            for j in range(m):
+        pre_col=image[sr][sc]
+        m=len(image)
+        n=len(image[0])
+        new=[[0]*n for _ in range(m)]
+        vis=[[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
                 new[i][j]=image[i][j]
-        self.dfs(sr,sc,image,new,in_color,color,n,m)
+        self.dfs(image,new,sr,sc,color,m,n,pre_col,vis)
         return new
 
         
