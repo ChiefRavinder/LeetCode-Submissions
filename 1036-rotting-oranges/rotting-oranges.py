9f7b85d552fd41ccd93f78291cdcb859cf5d04f1
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2024-02-15 03:56:30 +0530

    Time: 46 ms (85.81%) | Memory: 16.6 MB (79.68%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,39 +2,37 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m=len(grid)
         n=len(grid[0])
-        vis=[[0]*n for _ in range(m)]
-        new=[[0]*n for _ in range(m)]
+        rott=[[0]*n for _ in range(m)]
         time=0
         for i in range(m):
             for j in range(n):
-                new[i][j]=grid[i][j]
-                    
+                rott[i][j]=grid[i][j]
         q=[]
+        vis=[[0]*n for _ in range(m)] 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==2:
+                if rott[i][j]==2:
                     vis[i][j]=1
                     q.append((i,j,0))
         while q:
-            row=q[0][0]
-            col=q[0][1]
-            t=q[0][2]
-            q.pop(0)
-            delrow=[0,1,0,-1]
-            delcol=[1,0,-1,0]
-            for i in range(4):
-                nrow=row+delrow[i]
-                ncol=col+delcol[i]
-                if 0<=nrow<m and 0<=ncol<n and vis[nrow][ncol]!=1 and grid[nrow][ncol]==1 and new[nrow][ncol]!=2:
-                    vis[nrow][ncol]=1
-                    new[nrow][ncol]=2
+            node=q.pop(0)
+            r=node[0]
+            c=node[1]
+            t=node[2]
+            dr=[1,0,-1,0]
+            dc=[0,1,0,-1]
+            for k in range(4):
+                nr=dr[k]+r
+                nc=dc[k]+c
+                if 0<=nr<m and 0<=nc<n and vis[nr][nc]!=1 and rott[nr][nc]==1:
+                    vis[nr][nc]=1
+                    rott[nr][nc]=2
                     nt=t+1
-                    q.append((nrow,ncol,nt))
+                    q.append((nr,nc,nt))
                     time=max(time,nt)
         for i in range(m):
             for j in range(n):
-                if new[i][j]==1:
+                if rott[i][j]==1:
                     return -1
         return time
-        
-        
+                    
