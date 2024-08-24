253f3ac64f415dbebc0ac0564e88b7122acf98ea
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2024-08-24 22:45:15 +0530

    Time: 42 ms (16.64%) | Memory: 16.4 MB (68.67%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -8,22 +8,15 @@ class Solution:
             if n < 0:
                 return 0
             pick = arr[n] + f(n - 2, dp, arr)
-            nopick = f(n - 1, dp, arr)
+            nopick = 0 + f(n - 1, dp, arr)
             dp[n] = max(pick, nopick)
             return dp[n]
 
         n = len(nums)
         if n == 1:
             return nums[0]
-
-        # Handle two cases: 
-        # 1. Exclude the first house and include the last house
-        # 2. Include the first house and exclude the last house
-
-        dp1 = [-1] * (n - 1)  # DP array for the case where we exclude the last house
-        dp2 = [-1] * (n - 1)  # DP array for the case where we exclude the first house
-        
-        first = f(n - 2, dp1, nums[1:])  # Exclude the first house (nums[1:] part)
-        second = f(n - 2, dp2, nums[:-1])  # Exclude the last house (nums[:-1] part)
-        
+        dp1 = [-1] * (n - 1)
+        dp2 = [-1] * (n - 1)
+        first = f(n - 2, dp1, nums[1:])
+        second = f(n - 2, dp2, nums[:-1])
         return max(first, second)
