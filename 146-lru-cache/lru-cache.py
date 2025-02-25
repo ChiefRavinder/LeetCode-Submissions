0d4f4930bf29b2dbf775c98d8a204dcdebfe6355
Ravinder Sharma <106022307+ChiefRavinder@users.noreply.github.com>
2025-02-26 01:16:10 +0530

    Time: 153 ms (44.25%) | Memory: 78.5 MB (55.46%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,51 +1,44 @@
 class Node:
-    def __init__(self,key,val):
-        self.val=val
-        self.key=key
-        self.next=self.prev=None
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.cache={}
+        self.capacity = capacity
+        self.cache = {}
 
-        self.left,self.right=Node(0,0),Node(0,0)
+        # Dummy head and tail
+        self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
-        
-    #remove from left
-    def remove(self,node):
-        prv,nxt= node.prev, node.next
-        prv.next,nxt.prev=nxt,prv
 
-    #add to right
-    def add(self,node):
-        prv,nxt=self.right.prev,self.right
-        prv.next=node
-        nxt.prev=node
-        node.next,node.prev=nxt,prv
-        
+    # Remove node from the list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # Insert node at the right (most recently used)
+    def add(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.remove(self.cache[key])  # Move to most recently used
             self.add(self.cache[key])
             return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        
-        self.cache[key]=Node(key,value)
-        self.add(self.cache[key])
+            self.remove(self.cache[key])  # Remove old node
 
-        if len(self.cache)>self.capacity:
+        self.cache[key] = Node(key, value)
+        self.add(self.cache[key])  # Add as most recently used
+
+        if len(self.cache) > self.capacity:
             lru = self.left.next  # Store the least recently used node
-            self.remove(lru)  # Remove it from the doubly linked list
-            del self.cache[lru.key]  # Delete from the cache dictionary
-            
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            self.remove(lru)  # Remove it from the list
+            del self.cache[lru.key]  # Remove from cache dictionary
